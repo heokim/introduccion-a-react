4430f96ac46f8b0a4a2cd042bb6cf533b91af0d6
heokim <andresssshk@gmail.com>
2022-08-29 12:52:06 -0400

    Estilos con CSS

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { TodoCounter } from "./TodoCounter";
 import { TodoSearch } from "./TodoSearch.js";
 import { TodoList } from "./TodoList.js";
 import { TodoItem } from "./TodoItem.js";
-import { CreateTodoButtom } from "./CreateTodoButton.js";
+import { CreateTodoButton } from "./CreateTodoButton.js";
 
 const todos = [
-  { text: 'Cortar cebolla', completed: false },
+  { text: 'Cortar cebolla', completed: true },
   { text: 'Tormar el curso de intro a react', completed: false },
   { text: 'Llorar con la llorona', completed: false }
 ];
@@ -16,12 +16,18 @@ function App() {
     <>
       <TodoCounter />
       <TodoSearch />
+
       <TodoList>
         {todos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} />
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
         ))}
       </TodoList>
-      <CreateTodoButtom />
+
+      <CreateTodoButton />
     </>
   );
 }
--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,9 +1,10 @@
 import React from "react";
+import './CreateTodoButton.css'
 
-function CreateTodoButtom() {
+function CreateTodoButton(props) {
   return (
-    <button>+</button>
+    <button className="CreateTodoButton">+</button>
   );
 }
 
-export { CreateTodoButtom };
+export { CreateTodoButton };
